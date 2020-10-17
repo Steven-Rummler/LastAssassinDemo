299d7c7d605768c941a5280658b68080f4759947
Bryce-Rummler <rummler.b@gmail.com>
2020-10-17 17:10:23 -0600

    cors issues

--- scripts.js
+++ scripts.js
@@ -7,7 +7,7 @@ function createGame() {
 
 function joinGame() {
     // Rebuild content section
-    document.getElementById("content").innerHTML = '<div id="buttons"><button id="demoCode" type="button" class="btn btn-secondary custombtn">Demo Code: </button><input class="form-control custombtn" id="gameCode" placeholder="Enter Game Code"><button id="joinLobby" type="button" class="btn btn-success custombtn">Join Lobby</button><button id="mainMenu" type="button" class="btn btn-danger custombtn">Main Menu</button></div>'
+    document.getElementById("content").innerHTML = '<div id="buttons"><button id="demoCode" type="button" class="btn btn-secondary custombtn">Demo Code: abc</button><input class="form-control custombtn" id="gameCode" placeholder="Enter Game Code"><button id="joinLobby" type="button" class="btn btn-success custombtn">Join Lobby</button><button id="mainMenu" type="button" class="btn btn-danger custombtn">Main Menu</button></div>';
     // Reset event listeners
     document.getElementById("joinLobby").addEventListener("click", joinLobby);
     document.getElementById("mainMenu").addEventListener("click", mainMenu);
@@ -23,8 +23,25 @@ function mainMenu() {
 
 function joinLobby() {
     // Call API
+    const value = document.getElementById("gameCode").value;
+    console.log(value);
+    if (value === "")
+      return;
+    let url = "http://stevenrummler.com:8000/games/" + value;
     // Rebuild content section
-    document.getElementById("content").innerHTML = '<div id="buttons"><button id="hostName" type="button" class="btn btn-secondary custombtn">Host Name</button><button id="gameMode" type="button" class="btn btn-secondary custombtn">Game Mode</button><button id="numPlayers" type="button" class="btn btn-secondary custombtn">Numbers of Players</button><button id="mainMenu" type="button" class="btn btn-danger custombtn">Leave Game</button></div>'
+    document.getElementById("content").innerHTML = '<div id="buttons"><button id="hostName" type="button" class="btn btn-secondary custombtn">Host Name</button><button id="gameMode" type="button" class="btn btn-secondary custombtn">Game Mode</button><button id="numPlayers" type="button" class="btn btn-secondary custombtn">Numbers of Players</button><button id="mainMenu" type="button" class="btn btn-danger custombtn">Leave Game</button></div>';
+    // Display API data
+    const myHeaders = new Headers({'Content-Type':'application/json','Origin':'stevenrummler.com'});
+    const myRequest = new Request(url, {method:'GET',headers:myHeaders,mode:'cors',cache:'default',});
+    console.log(myRequest);
+    fetch(myRequest)
+        .then(function(response) {
+            return response.json();
+        }) .then(function(json) {
+            document.getElementById("hostName").innerHTML = "Host: " + json.host;
+            document.getElementById("gameMode").innerHTML = "gameMode: " + json.mode;
+            document.getElementById("numPlayers").innerHTML = "Players: " + json.players;
+        });
     // Reset event listeners
     document.getElementById("mainMenu").addEventListener("click", mainMenu);
 }
